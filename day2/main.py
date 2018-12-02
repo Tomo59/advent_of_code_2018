e347bd6a49a87e414948c0addaf54e4f58566500
Thomas Gambier <thomas.gambier@gmail.com>
2018-12-02 10:35:20 +0100

    better day2

--- day2/main.py
+++ day2/main.py
@@ -1,23 +1,24 @@
 #/usr/bin/env python3
 
 from collections import Counter
+from itertools import combinations
 
 def p1(IDs):
     two_letters = 0
     three_letters = 0
     for i in IDs:
-        letters = Counter(list(i))
+        letters = Counter(i)
         if 2 in letters.values():
-            two_letters = two_letters + 1
+            two_letters += 1
         if 3 in letters.values():
-            three_letters = three_letters + 1
+            three_letters += 1
     return two_letters * three_letters
 
 def compare(id1, id2):
     """return a tuple (Bool, int) to say if the 2 strings differ only by one letter and at which position"""
     diff = -1
-    for pos,l in enumerate(list(id1)):
-        if l != list(id2)[pos]:
+    for pos,l in enumerate(id1):
+        if l != id2[pos]:
             if diff == -1:
                 diff = pos
             else:
@@ -26,13 +27,10 @@ def compare(id1, id2):
 
 
 def p2(IDs):
-    for pos,i in enumerate(IDs):
-        for j in IDs[pos+1:]:
-            (differ_1_letter, diff_position) = compare(i,j)
-            if differ_1_letter:
-                l = list(j)
-                del l[diff_position]
-                return "".join(l)
+    for (id1, id2) in combinations(IDs, 2):
+        (differ_1_letter, diff_position) = compare(id1,id2)
+        if differ_1_letter:
+            return id1[:diff_position] + id1[diff_position+1:]
 
 if __name__ == "__main__":
     f = open('input.txt', 'r')
